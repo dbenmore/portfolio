5efd6284b71b76ee4586a72e6908f1a015b1f628
Daniel Benmore <dbenmore@daniels-mbp.home>
2020-04-09 23:14:27 +0100

    New design

--- scripts/build.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var pug = require('pug');
-
-// Compile the template to a function string
-var jsFunctionString = pug.compileFileClient('src/articles.pug', {name: "fancyTemplateFun"});
-
-// Maybe you want to compile all of your templates to a templates.js file and serve it to the client
-fs.writeFileSync("docs/article.html", jsFunctionString);
